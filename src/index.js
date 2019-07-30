264dc25eb2bded0166d356e5d9fbdf06beafb2b2
jaris <jarisma@foxmail.com>
2019-07-30 15:16:50 +0800

    实现TodoList的新增和删除功能

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 import React from 'react'; // 编译jsx语法
 import ReactDOM from 'react-dom'; // 将组件挂载到dom节点上
-import App from './App'; // 引入组件
+import TodoList from './TodoList'; // 引入组件
 // JSX语法中，如果我们要使用自己创建的组件，组件开头必须以大写字母开头
-ReactDOM.render( < App / > , document.getElementById('root'));
+ReactDOM.render( < TodoList / > , document.getElementById('root'));
