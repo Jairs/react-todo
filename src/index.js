96f149217ab85b974104a948394f638ef26a1d8f
jaris <jarisma@foxmail.com>
2019-07-30 14:41:11 +0800

    初始化项目

--- src/index.js
+++ src/index.js
@@ -1,12 +1,5 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
-
-ReactDOM.render(<App />, document.getElementById('root'));
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+import React from 'react'; // 编译jsx语法
+import ReactDOM from 'react-dom'; // 将组件挂载到dom节点上
+import App from './App'; // 引入组件
+// JSX语法中，如果我们要使用自己创建的组件，组件开头必须以大写字母开头
+ReactDOM.render( < App / > , document.getElementById('root'));
