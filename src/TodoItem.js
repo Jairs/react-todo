eb475a17b2ec1d648e5daf553d940836082812f2
jaris <jarisma@foxmail.com>
2019-07-30 18:51:35 +0800

    限制子组件接收数据的格式

--- src/TodoItem.js
+++ src/TodoItem.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react'
+import PropTypes from 'prop-types'
 
 class TodoItem extends Component {
 
@@ -23,6 +24,12 @@ class TodoItem extends Component {
     const { deleteItem, index } = this.props
     deleteItem(index)
   }
+}
+
+TodoItem.propTypes = {
+  content: PropTypes.string,
+  deleteItem: PropTypes.func,
+  index: PropTypes.number
 }
 
 export default TodoItem
