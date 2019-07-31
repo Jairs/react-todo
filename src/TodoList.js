5a52e86f5194baf77b836fe651b9001c696bc87c
jaris <jarisma@foxmail.com>
2019-07-31 19:06:23 +0800

    使用redux实现todoList的功能

--- src/TodoList.js
+++ src/TodoList.js
@@ -1,42 +1,27 @@
 import React, { Component, Fragment } from 'react'
-// Fragment 是react提供的占位符
-import TodoItem from './TodoItem'
 import axios from 'axios'
 
 import { Input, Button, List } from 'antd'
 import 'antd/dist/antd.css'
 
+import store from './store'
+
 class TodoList extends Component {
 
   constructor(props){
     super(props);
-    this.state = {
-      inputValue: '',
-      list: []
-    }
+    this.state = store.getState()
     this.handleInputChange = this.handleInputChange.bind(this)
     this.handleBtnClick = this.handleBtnClick.bind(this)
     this.handleItemDelete = this.handleItemDelete.bind(this)
-  }
-
-  componentDidMount(){
-    axios.get('/api/todolist').then((res)=>{
-      this.setState(()=>({
-        list: [...res.data]
-      }))
-    }).catch(()=>{
-      console.error('error')
-    })
-  }
+    this.handleStoreChange = this.handleStoreChange.bind(this)
+    store.subscribe(this.handleStoreChange) // store发生变化就会自动执行这个函数
+  } 
 
   render() {
     return (
       <Fragment>
-        {
-          //Fragment 是react提供的占位符
-        }
         <div style={{marginTop:'10px',marginLeft:'10px'}}>
-          {/* 下面是一个input输入框 */}
           <Input 
             id="insertArea"
             className='input'
@@ -49,59 +34,42 @@ class TodoList extends Component {
             type="primary"
             onClick={this.handleBtnClick}
           >提交</Button>
-          {/* <List
+          <List
             style={{marginTop:'10px',width:'300px'}}
             bordered
             dataSource={this.state.list}
-            renderItem={item => <List.Item>{item}</List.Item>}
-          /> */}
+            renderItem={(item,index) => <List.Item onClick={this.handleItemDelete(index)}>{item}</List.Item>}
+          />
         </div>
-
-        <ul>
-          {this.getTodoItem()}
-        </ul>
       </Fragment>
     )
   }
 
-  getTodoItem() {
-    return this.state.list.map((item, index) => {
-      return (
-        <TodoItem
-          key={index}
-          content={item} 
-          index={index}
-          deleteItem={this.handleItemDelete}
-        />
-      )
-    })
+  handleInputChange(e) {
+    const action = {
+      type:'change_input_value',
+      value:e.target.value
+    }
+    store.dispatch(action)
   }
 
-  handleInputChange(e) {
-    const value = e.target.value
-    this.setState(()=>({
-      inputValue: value
-    }))
+  handleStoreChange() {
+    this.setState(store.getState())
   }
 
   handleBtnClick() {
-    this.setState((prevState)=>({
-      list:[...prevState.list, prevState.inputValue],
-      inputValue: ''
-    }))
+    const action = {
+      type: 'add_todo_item'
+    }
+    store.dispatch(action)
   }
 
   handleItemDelete(index) {
-    // immutable
-    // state 不允许我们做任何的改变
-    
-    this.setState((prevState)=>{
-      const list = [...prevState.list]
-      list.splice(index, 1)
-      return {
-        list
-      }
-    })
+    const action = {
+      type: 'delete_todo_item',
+      index
+    }
+    store.dispatch(action)
   }
 }
 
