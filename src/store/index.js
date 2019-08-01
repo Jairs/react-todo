7e36a40d91199c0e9d999bf75c7cc11b1afae019
jaris <jarisma@foxmail.com>
2019-08-01 11:38:58 +0800

    添加备注

--- src/store/index.js
+++ src/store/index.js
@@ -1,6 +1,8 @@
 import {createStore} from 'redux'
 import reducer from './reducer'
 
+// store是唯一的
+// 只有store能够改变自己的内容
 const store = createStore(
   reducer,
   window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
